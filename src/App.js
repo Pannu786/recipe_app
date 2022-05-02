def714bb0101e3a2e966a9a7a1e67f65840ad917
Pannu786 <thepannu786@gmail.com>
2022-05-02 19:17:05 +0530

    added Searched cmp and other stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Pages from './pages/Pages';
-
+import Category from './components/Category';
+import Search from './components/Search';
 function App() {
   return (
     <div className='App'>
+      <Search />
+      <Category />
       <Pages />
     </div>
   );
-} 
+}
 
 export default App;
---
+++ src/pages/Searched.js
@@ -0,0 +1,14 @@
+const Searched = () => {
+  
+  const getSearched = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+
+  return <div>Searched</div>;
+};
+
+export default Searched;
