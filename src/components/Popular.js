df6705ec9fdfb8e0542e930df8b46fc455231d43
Pannu786 <thepannu786@gmail.com>
2022-05-03 12:54:25 +0530

    need to make it responsive, all dne

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/splide/dist/css/splide.min.css';
+import { Link } from 'react-router-dom';
 
 const Popular = () => {
   const [popular, setPopular] = useState([]);
@@ -20,7 +21,7 @@ const Popular = () => {
         `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
       );
       const data = await api.json();
-      
+
       localStorage.setItem('popular', JSON.stringify(data.recipes));
 
       setPopular(data.recipes);
@@ -44,9 +45,11 @@ const Popular = () => {
             return (
               <SplideSlide key={recipe.id}>
                 <Card>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
-                  <Gradient />
+                  <Link to={'/recipe/' + recipe.id}>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <Gradient />
+                  </Link>
                 </Card>
               </SplideSlide>
             );
